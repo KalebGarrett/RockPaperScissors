44e1b8be4fa3b103f3e436d1058a3ab0d38daa85
Kaleb Garrett <kalebagarrett@gmail.com>
2024-02-01 21:59:40 -0500

    Moved rps game into Bootstrap columns

--- RockPaperScissors.Web/wwwroot/js/site.js
+++ RockPaperScissors.Web/wwwroot/js/site.js
@@ -32,16 +32,16 @@ function computerTurn(){
 }
 
 function checkWinner(){
-    if(player == computer){
+    if(player === computer){
         return "Draw!";
     }
-    else if(computer == "ROCK"){
-        return (player == "PAPER") ? "You Win!" : "You Lose!"
+    else if(computer === "ROCK"){
+        return (player === "PAPER") ? "You Win!" : "You Lose!"
     }
-    else if(computer == "PAPER"){
-        return (player == "SCISSORS") ? "You Win!" : "You Lose!"
+    else if(computer === "PAPER"){
+        return (player === "SCISSORS") ? "You Win!" : "You Lose!"
     }
-    else if(computer == "SCISSORS"){
-        return (player == "ROCK") ? "You Win!" : "You Lose!"
+    else if(computer === "SCISSORS"){
+        return (player === "ROCK") ? "You Win!" : "You Lose!"
     }
 }
